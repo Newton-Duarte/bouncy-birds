5ffee8de1731c0de0e5b81910c3ef0c503b030b6
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-14 18:02:34 -0300

    feat: add bird script

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    Vector3 startPosition;
+    Rigidbody2D rb;
+    [SerializeField] float launchPower = 250;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FindAnyObjectByType<Enemy>(FindObjectsInactive.Exclude) == null)
+        {
+            Debug.Log("Game Over");
+            int levelToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        Vector3 directionAndMagnitude = startPosition - transform.position;
+        rb.AddForce(directionAndMagnitude * launchPower);
+        rb.gravityScale = 1;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        destination.z = 0;
+        transform.position = destination;
+    }
+}
