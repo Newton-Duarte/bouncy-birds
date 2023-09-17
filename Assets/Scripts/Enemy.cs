06999d053e2ddb86d0015b420c6a376e2343a557
Newton Duarte Cavalcanti <newton_@hotmail.com>
2023-09-17 17:28:51 -0300

    feat: add die method to enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] float destroyImpactMagnitude = 5;
+    [SerializeField] AudioSource sourceFx;
+    [SerializeField] AudioClip enemyHitFx;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Ground"))
         {
-            Destroy(gameObject);
+            Die();
         }
+
+        if (collision.relativeVelocity.magnitude > destroyImpactMagnitude)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        sourceFx.PlayOneShot(enemyHitFx);
+        Invoke(nameof(Destroy), 0.05f);
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
     }
 
     void OnBecameInvisible()
